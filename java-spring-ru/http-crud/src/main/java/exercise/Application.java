5956741a888518cc5c7ae20d40576f7874ca77c0
@hexlet/cli <support@hexlet.io>
2024-05-26 14:43:37 +0600

    submit java-spring-ru/http-crud

--- java-spring-ru/http-crud/src/main/java/exercise/Application.java
+++ java-spring-ru/http-crud/src/main/java/exercise/Application.java
@@ -27,47 +27,47 @@ public class Application {
     }
 
     // BEGIN
-//    GET /posts — список всех постов
-    @GetMapping("/posts")
-    public List<Post> showall(@RequestParam(defaultValue = "10") Integer limit) {
+//    GET/posts
+@GetMapping("/posts")
+public List<Post> getAllPosts(@RequestParam(defaultValue = "10")Integer limit){
         return posts.stream().limit(limit).toList();
+}
+
+//GET/posts/{id}
+@GetMapping ("/posts/{id}")
+public Optional<Post> show(@PathVariable String id){
+        var post = posts.stream()
+                .filter(p -> p.getId().equals(id))
+                .findFirst();
+        return post;
     }
 
-//    GET /posts/{id} — просмотр конкретного поста
-@GetMapping("/posts/{id}") // Вывод страницы
-public Optional<Post> show(@PathVariable String id) {
-    var post = posts.stream()
-            .filter(p -> p.getId().equals(id))
-            .findFirst();
-    return post;
-}
-
-//    POST /posts – создание нового поста
-@PostMapping("/posts") // Создание страницы
-public Post create(@RequestBody Post post) {
-    posts.add(post);
-    return post;
-}
-
-//    PUT /posts/{id} – обновление поста
-@PutMapping("/posts/{id}") // Обновление страницы
-public Post update(@PathVariable String id, @RequestBody Post data) {
-    var maybePost = posts.stream()
-            .filter(p -> p.getId().equals(id))
-            .findFirst();
-    if (maybePost.isPresent()) {
-        var post = maybePost.get();
-        post.setId(data.getId());
-        post.setTitle(data.getId());
-        post.setBody(data.getBody());
+//    POST/posts
+    @PostMapping("/posts")
+    public Post create(@RequestBody Post post){
+        posts.add(post);
+        return post;
     }
-    return data;
-}
 
-//    DELETE /posts/{id} – удаление поста
+//    PUT/posts/id
+    @PutMapping("/posts/{id}")
+public Post update(@PathVariable String id, @RequestBody Post data){
+        var maybePost = posts.stream()
+                .filter(p -> p.getId().equals(id))
+                .findFirst();
+        if (maybePost.isPresent()){
+            var post = maybePost.get();
+            post.setId(data.getId());
+            post.setTitle(data.getTitle());
+            post.setBody(data.getBody());
+        }
+        return data;
+    }
+
+//    DELETE/posts/{id}
 @DeleteMapping("/posts/{id}")
-public void delete(@PathVariable String id) {
-    posts.removeIf(p -> p.getId().equals(id));
+public void deletePost(@PathVariable String id){
+        posts.removeIf(p->p.getId().equals(id));
 }
-    // END
+// END
 }
